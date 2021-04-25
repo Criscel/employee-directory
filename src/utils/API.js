31c67b0694b54a12288ea70e31b60cc1efbde131
Criscel Agdeppa <cel_agdeppa@yahoo.com>
2021-04-25 16:53:48 +1000

    adding index.js in navigation component

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export default {
-    getUser: function() {
+const API = {
+    getEmployee: function() {
         return axios.get("https://randomuser.me/api/?results=50&nat=au");
     },
 };
